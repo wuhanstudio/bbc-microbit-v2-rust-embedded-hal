e63dc043a839e933601b969aaefbe5f77289f192
wuhanstudio <wuhanstudios@gmail.com>
2025-09-09 20:30:55 +0100

    Add rtt-device example

---
+++ hello-rtt-print/src/main.rs
@@ -0,0 +1,25 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use embedded_hal::delay::DelayNs;
+use panic_halt as _;
+
+use nrf52833_hal::Timer;
+
+// Debugging via RTT, no serrial port needed
+use rtt_target::rtt_init_print;
+use rtt_target::rprintln;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+
+    let p = nrf52833_hal::pac::Peripherals::take().unwrap();
+    let mut timer = Timer::new(p.TIMER0);
+
+    loop {
+        rprintln!("Hello, world!");
+        timer.delay_ms(1000_u32);
+    }
+}
