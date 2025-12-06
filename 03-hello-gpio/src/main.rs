3554b010dd17e94db322fc942f591cceb987bf9d
wuhanstudio <wuhanstudios@gmail.com>
2025-12-06 22:00:44 +0000

    [hello-gpio] Rename _col1 to col1

--- 03-hello-gpio/src/main.rs
+++ 03-hello-gpio/src/main.rs
@@ -21,19 +21,19 @@ fn main() -> ! {
     let p0 = gpio::p0::Parts::new(peripherals.P0);
 
     let _row1 = p0.p0_21.into_push_pull_output(gpio::Level::High);
-    let mut _col1 = p0.p0_28.into_push_pull_output(gpio::Level::Low);
+    let mut col1 = p0.p0_28.into_push_pull_output(gpio::Level::Low);
 
     let mut button_a = p0.p0_14.into_pullup_input();
 
     loop {
         if button_a.is_low().unwrap() {
             rtt_target::rprintln!("Button A pressed");
-            _col1.set_low().unwrap();
+            col1.set_low().unwrap();
 
             // DEBUG via defmt
             // defmt::info!("Button A pressed");
         } else {
-            _col1.set_high().unwrap();
+            col1.set_high().unwrap();
             rtt_target::rprintln!("Button A released");
 
             // DEBUG via defmt
