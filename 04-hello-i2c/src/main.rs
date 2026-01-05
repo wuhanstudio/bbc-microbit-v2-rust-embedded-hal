c3d700f5e1864020c2fdd2373bf23cf6d942522f
wuhanstudio <wuhanstudios@gmail.com>
2026-01-05 13:22:14 +0000

    [hello-i2c] Use standard embedded-hal functions

--- 04-hello-i2c/src/main.rs
+++ 04-hello-i2c/src/main.rs
@@ -3,35 +3,28 @@
 
 use cortex_m_rt::entry;
 use cortex_m::asm::wfi;
+use panic_halt as _;
 
+use embedded_hal::i2c::I2c;
 use nrf52833_hal::{pac, gpio, twim::*};
 
 // Debugging via RTT, no serrial port needed
 use rtt_target::rtt_init_print;
-
-use core::panic::PanicInfo;
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    rtt_target::rprintln!("Panic occurred: {}", _info);
-    loop {}
-}
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
 
     let peripherals = pac::Peripherals::take().unwrap();
-    
 
     let p0 = gpio::p0::Parts::new(peripherals.P0);
-
     let scl = p0.p0_08.into_floating_input().degrade();
     let sda = p0.p0_16.into_floating_input().degrade();
 
-    let mut i2c = Twim::new(peripherals.TWIM0, Pins { scl, sda }, Frequency::K100);
+    let mut twim = Twim::new(peripherals.TWIM0, Pins { scl, sda }, Frequency::K100);
 
-    // Scan I2C bus from address 0x00 to 0x80
-    rtt_target::rdbg!("Scanning I2C bus from 0x00 to 0x80...");
+    rtt_target::rprintln!("Start i2c scanning...");
+    rtt_target::rprintln!();
 
     // Print header
     for header in 0x0..0x10 {
@@ -39,13 +32,13 @@ fn main() -> ! {
     }
     rtt_target::rprintln!();
 
+    // 0x00 -> 0x7F
     for addr in 0x00..0x80 {
+        // Write the empty array and check the slave response.
         let byte: [u8; 1] = [0; 1];
-        let res = i2c.write(addr, &byte);
-        if res.is_ok() {
+        if twim.write(addr, &byte).is_ok() {
             rtt_target::rprint!("{:02x}", addr);
-        }
-        else {
+        } else {
             rtt_target::rprint!("..");
         }
         if addr % 0x10 == 0x0F {
@@ -55,20 +48,23 @@ fn main() -> ! {
         }
     }
 
-    let mut acc = [0u8];
-    let mut mag = [0u8];
+    rtt_target::rprintln!();
+    rtt_target::rprintln!("Done!");
 
     const ACCELEROMETER_ADDR: u8 = 0b0011001; // 0x19
-    const MAGNETOMETER_ADDR: u8 = 0b0011110; // 0x1e
+    const MAGNETOMETER_ADDR: u8 = 0b0011110; // 0x1E
 
-    // Do NOT use const here (EasyDMA limitation)
-    let ACCELEROMETER_ID_REG: u8 = 0x0f;
-    let MAGNETOMETER_ID_REG: u8 = 0x4f;
+    const ACCELEROMETER_ID_REG: u8 = 0x0f;
+    const MAGNETOMETER_ID_REG: u8 = 0x4f;
 
-    i2c.write_then_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc).unwrap();
+    let mut acc = [0_u8];
+    let mut mag = [0_u8];
+
+    // First write the address + register onto the bus, then read the chip's responses
+    twim.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc).unwrap();
+    twim.write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag).unwrap();
+
     rtt_target::rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
-
-    i2c.write_then_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag).unwrap();
     rtt_target::rprintln!("The magnetometer chip's id is: {:#b}", mag[0]);
 
     loop {
