777afcca6fb690caac307e4c951f314dec84a3e6
wuhanstudio <wuhanstudios@gmail.com>
2026-04-14 11:12:31 +0100

    [hello-state-machine] Add LED GPIO

--- 09-hello-state-machine/src/blinky.rs
+++ 09-hello-state-machine/src/blinky.rs
@@ -7,17 +7,22 @@ use crate::ticker::Ticker;
 use crate::timer::Timer;
 
 use rtt_target::rprintln;
+use embedded_hal::digital::{InputPin, OutputPin};
 
 // #[derive(Default)]
-pub struct Blinky
+pub struct Blinky<P: OutputPin, const N: usize>
 {
     pub timer: Timer,
+    pub rows: [P; N],
+    pub cols: [P; N],
 }
 
-impl Default for Blinky {
-    fn default() -> Self {
+impl<P: OutputPin, const N: usize> Blinky<P, N> {
+    pub fn new(rows: [P; N], cols: [P; N]) -> Self {
         Self {
-            timer: Timer::new(1000.millis())
+            timer: Timer::new(1000.millis()),
+            rows,
+            cols,
         }
     }
 }
@@ -27,7 +32,7 @@ pub enum Event {
 }
 
 #[state_machine(initial = "State::led_on()")]
-impl Blinky {
+impl<P: OutputPin, const N: usize> Blinky<P, N> {
     #[state(entry_action = "enter_led_on", exit_action = "exit_led_on")]
     fn led_on(event: &Event) -> Outcome<State> {
         match event {
@@ -47,6 +52,13 @@ impl Blinky {
     #[action]
     fn enter_led_on(&mut self) {
         rprintln!("LED ON");
+        for row in self.rows.iter_mut() {
+            row.set_high().unwrap();
+        }
+
+        for col in self.cols.iter_mut() {
+            col.set_high().unwrap();
+        }
         self.timer = Timer::new(1000.millis());
     }
 
@@ -58,6 +70,13 @@ impl Blinky {
     #[action]
     fn enter_led_off(&mut self) {
         rprintln!("LED OFF");
+        for row in self.rows.iter_mut() {
+            row.set_high().unwrap();
+        }
+
+        for col in self.cols.iter_mut() {
+            col.set_low().unwrap();
+        }
         self.timer = Timer::new(1000.millis());
     }
 
@@ -67,7 +86,7 @@ impl Blinky {
     }
 }
 
-pub fn blinky_poll(blinky_task: &mut InitializedStateMachine<Blinky>) {
+pub fn blinky_poll<P: OutputPin, const N: usize>(blinky_task: &mut InitializedStateMachine<Blinky<P, N>>) {
     if blinky_task.timer.is_ready() {
         let time = Ticker::now();
         rprintln!("Blinky Event triggered at {} ticks, {} ms", time.ticks(), time.duration_since_epoch().to_millis());
--- 09-hello-state-machine/src/main.rs
+++ 09-hello-state-machine/src/main.rs
@@ -3,7 +3,9 @@
 
 use panic_halt as _;
 use cortex_m_rt::entry;
+
 use nrf52833_hal as hal;
+use nrf52833_hal::gpio;
 
 use rtt_target::{rprintln, rtt_init_print};
 
@@ -29,9 +31,25 @@ fn main() -> ! {
     clocks.start_lfclk();
 
     let mut cp = hal::pac::CorePeripherals::take().unwrap();
+    let p0 = gpio::p0::Parts::new(p.P0);
+    let p1 = gpio::p1::Parts::new(p.P1);
+
+    let r1 = p0.p0_21.into_push_pull_output(gpio::Level::High).degrade();
+    let r2 = p0.p0_22.into_push_pull_output(gpio::Level::High).degrade();
+    let r3 = p0.p0_15.into_push_pull_output(gpio::Level::High).degrade();
+    let r4 = p0.p0_24.into_push_pull_output(gpio::Level::High).degrade();
+    let r5 = p0.p0_19.into_push_pull_output(gpio::Level::High).degrade();
+    let rows = [r1, r2, r3, r4, r5];
+
+    let c1 = p0.p0_28.into_push_pull_output(gpio::Level::Low).degrade();
+    let c2: gpio::Pin<gpio::Output<gpio::PushPull>> = p0.p0_11.into_push_pull_output(gpio::Level::Low).degrade();
+    let c3: gpio::Pin<gpio::Output<gpio::PushPull>> = p0.p0_31.into_push_pull_output(gpio::Level::Low).degrade();
+    let c4: gpio::Pin<gpio::Output<gpio::PushPull>> = p1.p1_05.into_push_pull_output(gpio::Level::Low).degrade();
+    let c5: gpio::Pin<gpio::Output<gpio::PushPull>> = p0.p0_30.into_push_pull_output(gpio::Level::Low).degrade();
+    let cols = [c1, c2, c3, c4, c5];
 
     Ticker::init(p.RTC0, &mut cp.NVIC);
-    let mut blinky_task: InitializedStateMachine<Blinky> = Blinky::default().uninitialized_state_machine().init();
+    let mut blinky_task: InitializedStateMachine<Blinky<_, 5>> = Blinky::new(rows, cols).uninitialized_state_machine().init();
 
     rprintln!("Waiting for events at {} ms", Ticker::now().duration_since_epoch().to_millis());
     loop {
